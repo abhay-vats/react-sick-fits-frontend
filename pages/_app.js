f2fb13e6699f5adf7cc73d0a41fa36bf4cdad9e9
Abhay Vats <abhayvats@pm.me>
2022-10-05 20:34:11 +0530

    Add Apollo

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,11 @@
 import React from 'react';
 import NProgress from 'nprogress';
 import Router from 'next/router';
+import { ApolloProvider } from '@apollo/client';
 
 // Local dependencies
 import Page from '../components/Page';
+import withData from '../lib/withData';
 
 // Styles
 import '../components/styles/nprogress.css';
@@ -14,10 +16,25 @@ Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
 // Main component
-const App = ({ Component, pageProps }) => (
-  <Page>
-    <Component {...pageProps} />
-  </Page>
-);
+function App({ Component, pageProps, apollo }) {
+  console.log(apollo);
+  return (
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
+  );
+}
 
-export default App;
+App.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+
+  return { pageProps };
+};
+
+export default withData(App);
