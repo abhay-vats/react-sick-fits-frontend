6894f25de04b54de6575941ec7cdf39c83e2044a
Abhay Vats <abhayvats@pm.me>
2022-10-09 16:23:07 +0530

    Add styles to single product page

--- components/SingleProduct.js
+++ components/SingleProduct.js
@@ -1,10 +1,12 @@
 // External dependencies
 import { gql, useQuery } from '@apollo/client';
 import Head from 'next/head';
+import styled from 'styled-components';
 
 // Local dependencies
 import DisplayError from './ErrorMessage';
 
+// Query
 const SINGLE_PRODUCT_QUERY = gql`
   query SINGLE_PRODUCT_QUERY($id: ID!) {
     Product(where: { id: $id }) {
@@ -19,6 +21,20 @@ const SINGLE_PRODUCT_QUERY = gql`
         }
       }
     }
+  }
+`;
+
+// Styles
+const ProductStyles = styled.div`
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  max-width: var(--maxWidth);
+  gap: 2rem;
+
+  img {
+    width: 100%;
+    object-fit: contain;
   }
 `;
 
@@ -43,7 +59,7 @@ const SingleProduct = ({ id }) => {
   } = data;
 
   return (
-    <div>
+    <ProductStyles>
       <Head>
         <title>Sick Fits | {name}</title>
       </Head>
@@ -52,7 +68,7 @@ const SingleProduct = ({ id }) => {
         <h2>{name}</h2>
         <p>{description}</p>
       </div>
-    </div>
+    </ProductStyles>
   );
 };
 
