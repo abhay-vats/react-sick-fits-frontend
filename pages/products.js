dcca62c5460c082699c2d41a6f8a38a6409e1581
Abhay Vats <abhayvats@pm.me>
2022-10-13 01:24:38 +0530

    Add pagination

--- pages/products.js
+++ pages/products.js
@@ -1,9 +1,16 @@
 // External dependencies
 import React from 'react';
+import Pagination from '../components/Pagination';
 
 // Local dependencies
 import Products from '../components/Products';
 
-const ProductsPage = () => <Products />;
+const ProductsPage = () => (
+  <>
+    <Pagination page={1} />
+    <Products />
+    <Pagination page={1} />
+  </>
+);
 
 export default ProductsPage;
